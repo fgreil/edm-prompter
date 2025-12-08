8db530cbe251bcc561ecacc5965f3d066d182507
fgreil <f.greil@tum.de>
2025-12-08 13:49:30 +0100

    Delete app directory

--- app/styles/appStyles.js
+++
@@ -1,8 +0,0 @@
-/** Globally used styles in app are declared here */
-
-import {  StyleSheet } from 'react-native';
-export const appStyles = StyleSheet.create({
-    rootView: {
-        flex: 1
-    }
-});
--- app/utils/colors.js
+++
@@ -1,9 +0,0 @@
-export const green = "#38C780";
-export const red = "#f44336";
-export const purple = "#6a1b9a";
-export const black = "#000";
-export const gray = "#878C93";
-export const white = "#FFFFFF";
-export const darkpurple = "#4F2A6f";
-export const grey = "#EDEDED";
-export const transparent = "rgba(0, 0, 0, 0.3)";
