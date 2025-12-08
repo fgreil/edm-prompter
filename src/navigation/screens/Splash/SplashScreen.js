597d107c12874aa46f21d0bea7428cb8f35a18c0
fgreil <f.greil@tum.de>
2025-12-08 14:06:06 +0100

    Create SplashScreen.js

---
+++ src/navigation/screens/Splash/SplashScreen.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { View, Image, StatusBar } from "react-native";
+import styles from "./styles";
+import * as colors from "../../../utils/colors";
+
+export default function SplashScreen({ navigation }) {
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace("App");  // IMPORTANT: replace() prevents BACK navigation
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar translucent={false} backgroundColor={colors.purple} />
+
+      <Image
+        resizeMode="contain"
+        source={require("../../../images/splash_icon.png")}
+        style={styles.imageStyle}
+      />
+    </View>
+  );
+}
