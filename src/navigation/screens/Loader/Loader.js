95a8a6f8dcc5147a3f51beb8153f97b84117aae3
fgreil <f.greil@tum.de>
2025-12-08 14:36:05 +0100

    Create Loader.js

---
+++ src/navigation/screens/Loader/Loader.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, ActivityIndicator, Modal } from "react-native";
+import styles from "./styles";
+import * as colors from "../../utils/colors";
+
+export default function Loader({ visible }) {
+  if (!visible) return null;
+
+  return (
+    <Modal
+      animationType="fade"
+      transparent
+      visible={visible}
+      presentationStyle="overFullScreen"
+    >
+      <View style={styles.viewStyle} pointerEvents="none">
+        <ActivityIndicator size="large" color={colors.white} />
+      </View>
+    </Modal>
+  );
+}
