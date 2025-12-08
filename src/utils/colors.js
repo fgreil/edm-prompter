c8fb18749238f3cbea402a0366fc26b15a964a73
fgreil <f.greil@tum.de>
2025-12-08 14:25:47 +0100

    Create colors.js

---
+++ src/utils/colors.js
@@ -0,0 +1,7 @@
+// src/utils/colors.js
+export const purple = "#6A1B9A";
+export const grey = "#F2F2F2";
+export const gray = "#A0A0A0";
+export const white = "#FFFFFF";
+export const black = "#000000";
+export const transparent = "transparent";
