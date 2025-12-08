1467215f445f77d07db8a128f6bba51d3805ca08
fgreil <f.greil@tum.de>
2025-12-08 14:15:42 +0100

    Update styles.js

--- src/navigation/screens/Search/styles.js
+++ src/navigation/screens/Search/styles.js
@@ -1,157 +1,86 @@
-import { StyleSheet, Platform } from "react-native";
-import * as FontSizes from "../../../utils/fontsSizes";
+// src/navigation/screens/Search/styles.js
+
+import { StyleSheet } from "react-native";
 import * as colors from "../../../utils/colors";
 
-export default (styles = StyleSheet.create({
+export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.grey
+    backgroundColor: colors.grey,
   },
+
+  // --------------------------
+  // HEADER
+  // --------------------------
   headerStyles: {
     flexDirection: "row",
     backgroundColor: colors.purple,
-    justifyContent: "space-around",
     alignItems: "flex-end",
-    paddingBottom: 6
-  },
-  welcomeTxt: {
-    alignSelf: "center",
-    fontSize: FontSizes.medium,
-    marginTop: 40
+    paddingBottom: 6,
+    paddingHorizontal: 10,
   },
   menuIcon: {
-    backgroundColor: colors.purple,
     marginHorizontal: 10,
-    marginBottom: 2
+    marginBottom: 2,
+  },
+  textInputViewStyle: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+    borderBottomWidth: 2,
+    borderBottomColor: colors.white,
+    marginBottom: 5,
+    paddingLeft: 5,
   },
   headerTextInputStyle: {
     flex: 1,
     color: colors.white,
     fontSize: 16,
-    paddingLeft: 5,
-    paddingBottom: 5
+    paddingBottom: 5,
   },
-  textInputViewStyle: {
-    flexDirection: "row",
-    flex: 2,
-    justifyContent: "space-around",
-    alignItems: "center",
-    borderBottomWidth: 2,
-    marginBottom: 5,
-    borderBottomColor: colors.white
-  },
+
+  // --------------------------
+  // RESULT CARDS
+  // --------------------------
   cardStyle: {
     marginHorizontal: 20,
     marginVertical: 15,
     backgroundColor: colors.white,
-    paddingVertical: 15
+    paddingVertical: 15,
+    borderRadius: 4,
+    elevation: 1,
   },
   cardHeaderStyle: {
     flexDirection: "row",
-    justifyContent: "flex-start",
     alignItems: "center",
-    flexWrap: "wrap"
   },
   cardHeaderImageStyle: {
     height: 50,
     width: 50,
-    marginHorizontal: 10
+    marginHorizontal: 10,
+    borderRadius: 4,
   },
   headerTitleStyle: {
     fontSize: 16,
-    fontWeight: "100",
-    flexWrap: "wrap",
     flex: 1,
-    paddingLeft: 5
   },
   cardTeaserViewStyle: {
-    flexWrap: "wrap",
-    backgroundColor: colors.white,
-    padding: 20
+    paddingHorizontal: 20,
+    paddingTop: 10,
   },
   cardTeaserTextStyle: {
-    flexWrap: "wrap",
-    fontSize: 15
-  },
-  modalStyle: {
-    margin: 0,
-    padding: 0
-  },
-  modalViewStyle: {
-    backgroundColor: colors.white,
-    borderRadius: 5,
-    marginHorizontal: 10,
-    paddingBottom: 5
-  },
-  modalHeaderTextStyle: {
-    color: colors.purple,
-    fontSize: 17,
-    fontWeight: "300",
-    marginTop: 20,
-    paddingLeft: 22
-  },
-  tagsViewStyle: {
-    padding: 12,
-    flexDirection: "row",
-    flexWrap: "wrap"
-  },
-  tagsTextViewStyle: {
-    borderWidth: 1,
-    alignSelf: "flex-start",
-    borderColor: colors.purple,
-    borderRadius: 25,
-    backgroundColor: colors.white,
-    paddingVertical: 10,
-    paddingHorizontal: 14,
-    marginTop: 10,
-    marginLeft: 6
-  },
-  tagsTextStyle: {
     fontSize: 15,
-    fontWeight: "100"
+    color: colors.black,
   },
-  modalBottomViewStyle: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    borderTopColor: colors.gray,
-    borderTopWidth: 2,
-    paddingTop: 10
-  },
-  modalBottomViewTextViewStyle: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center"
-  },
-  modalBottomButtonStyle: {
-    marginHorizontal: 15,
-    marginVertical: 12,
-    fontSize: 15,
-    color: colors.purple,
-    fontWeight: "100"
-  },
+
+  // --------------------------
+  // EMPTY STATE
+  // --------------------------
   blankScreenStyle: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingTop: 100
-  },
-  largeIconStyle: {
-    marginVertical: 35,
-    position: "absolute"
-  },
-  blankScreenHeaderTextStyle: {
-    fontSize: 17,
-    fontWeight: "600",
-    color: colors.gray,
-    textAlign: "center"
-  },
-  detailTextStyle: {
-    fontSize: 16,
-    fontWeight: "100",
-    color: colors.gray,
-    paddingVertical: 16,
-    textAlign: "center"
+    paddingHorizontal: 30,
   },
   iconContainerStyle: {
     width: 80,
@@ -161,16 +90,34 @@ export default (styles = StyleSheet.create({
     borderRadius: 50,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 40
+    marginBottom: 40,
   },
-  labelStyle: {
-    color: colors.purple,
-    marginLeft: 12,
+  blankScreenHeaderTextStyle: {
+    fontSize: 17,
+    fontWeight: "600",
+    color: colors.gray,
+    textAlign: "center",
+  },
+  detailTextStyle: {
     fontSize: 16,
-    fontWeight: "100"
+    color: colors.gray,
+    textAlign: "center",
+    marginTop: 16,
   },
-  filterViewStyle: {
-    margin: 8
+
+  // --------------------------
+  // HISTORY
+  // --------------------------
+  searchTopTextStyle: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    fontSize: 16,
+    fontWeight: "600",
+    color: colors.black,
   },
-  cancelIcon: {}
-}));
+  listTextStyle: {
+    fontSize: 15,
+    color: colors.black,
+    flexShrink: 1,
+  },
+});
