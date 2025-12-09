7fa0337a5a16d0c317f9b8290bed0cf4fd90c033
fgreil <f.greil@tum.de>
2025-12-09 01:29:49 +0100

    Update styles.js

--- src/navigation/screens/Search/styles.js
+++ src/navigation/screens/Search/styles.js
@@ -1,87 +1,47 @@
-// src/navigation/screens/Search/styles.js
-
 import { StyleSheet } from "react-native";
 import * as colors from "../../../utils/colors";
 
 export default StyleSheet.create({
+  // Container (not always needed, but kept for flexibility)
   container: {
     flex: 1,
     backgroundColor: colors.grey,
   },
 
-  // --------------------------
-  // HEADER
-  // --------------------------
-  headerStyles: {
-    flexDirection: "row",
-    backgroundColor: colors.purple,
-    alignItems: "flex-end",
-    paddingBottom: 6,
-    paddingHorizontal: 10,
-  },
-  menuIcon: {
-    marginHorizontal: 10,
-    marginBottom: 2,
-  },
-  textInputViewStyle: {
-    flexDirection: "row",
-    flex: 1,
-    alignItems: "center",
-    borderBottomWidth: 2,
-    borderBottomColor: colors.white,
-    marginBottom: 5,
-    paddingLeft: 5,
-  },
-  headerTextInputStyle: {
-    flex: 1,
-    color: colors.white,
+  // "Previously searched" header
+  searchTopTextStyle: {
     fontSize: 16,
-    paddingBottom: 5,
-  },
-
-  // --------------------------
-  // RESULT CARDS
-  // --------------------------
-  cardStyle: {
-    marginHorizontal: 20,
-    marginVertical: 15,
-    backgroundColor: colors.white,
+    fontWeight: "200",
+    paddingLeft: 16,
     paddingVertical: 15,
-    borderRadius: 4,
-    elevation: 1,
-  },
-  cardHeaderStyle: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  cardHeaderImageStyle: {
-    height: 50,
-    width: 50,
-    marginHorizontal: 10,
-    borderRadius: 4,
-  },
-  headerTitleStyle: {
-    fontSize: 16,
-    flex: 1,
-  },
-  cardTeaserViewStyle: {
-    paddingHorizontal: 20,
-    paddingTop: 10,
-  },
-  cardTeaserTextStyle: {
-    fontSize: 15,
     color: colors.black,
   },
 
-  // --------------------------
-  // EMPTY STATE
-  // --------------------------
+  // Each row in the list
+  historyRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 12,
+    paddingVertical: 14,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.gray,
+    backgroundColor: colors.white,
+  },
+
+  listTextStyle: {
+    fontSize: 15,
+    fontWeight: "300",
+    color: colors.black,
+  },
+
+  // Empty history screen
   blankScreenStyle: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 30,
+    paddingHorizontal: 20,
   },
+
   iconContainerStyle: {
     width: 80,
     height: 80,
@@ -92,32 +52,24 @@ export default StyleSheet.create({
     alignItems: "center",
     marginBottom: 40,
   },
+
   blankScreenHeaderTextStyle: {
     fontSize: 17,
     fontWeight: "600",
     color: colors.gray,
     textAlign: "center",
   },
+
   detailTextStyle: {
     fontSize: 16,
+    fontWeight: "100",
     color: colors.gray,
+    paddingVertical: 16,
     textAlign: "center",
-    marginTop: 16,
   },
 
-  // --------------------------
-  // HISTORY
-  // --------------------------
-  searchTopTextStyle: {
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    fontSize: 16,
-    fontWeight: "600",
-    color: colors.black,
-  },
-  listTextStyle: {
-    fontSize: 15,
-    color: colors.black,
-    flexShrink: 1,
+  largeIconStyle: {
+    marginVertical: 35,
+    position: "absolute",
   },
 });
