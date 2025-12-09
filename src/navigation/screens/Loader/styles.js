3ff97786a477c37e60a6046c03fae7adc88ab939
fgreil <f.greil@tum.de>
2025-12-09 01:43:49 +0100

    Update styles.js

--- src/navigation/screens/Loader/styles.js
+++ src/navigation/screens/Loader/styles.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import * as colors from "../../utils/colors";
+import * as colors from "../../../utils/colors";
 
 export default StyleSheet.create({
   viewStyle: {
