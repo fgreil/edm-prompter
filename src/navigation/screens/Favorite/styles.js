53b90861ab782e2dfcb91260ecf1cc864020974e
fgreil <f.greil@tum.de>
2025-12-09 02:19:03 +0100

    Update styles.js

--- src/navigation/screens/Favorite/styles.js
+++ src/navigation/screens/Favorite/styles.js
@@ -1,56 +1,78 @@
 // src/navigation/screens/Favorite/styles.js
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import * as colors from "../../../utils/colors";
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.grey,
-    paddingTop: 10,
+  container: { 
+    flex: 1, 
+    backgroundColor: colors.grey 
   },
 
-  // Each favorite card container
+  menuIcon: {
+    marginRight: 10,
+    backgroundColor: colors.purple,
+    marginLeft: 10
+  },
+
   containerListStyle: {
+    flex: 1,
     backgroundColor: colors.white,
     marginHorizontal: 10,
     marginVertical: 6,
     borderRadius: 8,
     elevation: 1,
-    overflow: "hidden",
+    zIndex: 99
   },
 
-  // Article title inside the card
   titleListStyle: {
-    paddingVertical: 20,
-    paddingHorizontal: 22,
+    marginVertical: 20,
+    marginLeft: 22,
     fontSize: 16,
-    color: colors.black,
+    fontWeight: "200",
+    color: colors.black
   },
 
-  // Swipeout right-side "Remove" panel
   removeStyle: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: colors.purple,
     justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 10,
+    alignItems: "center"
   },
 
   removeTextStyle: {
     color: colors.white,
+    textAlign: "center",
     fontSize: 14,
-    marginTop: 6,
+    fontWeight: "100"
   },
-
-  // -----------------------------------------
-  // Blank / empty state styles
-  // -----------------------------------------
 
   blankScreenStyle: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 30,
+    textAlign: "center",
+    paddingHorizontal: 20
+  },
+
+  largeIconStyle: {
+    marginVertical: 35,
+    position: "absolute"
+  },
+
+  blankScreenHeaderTextStyle: {
+    fontSize: 17,
+    fontWeight: "600",
+    color: colors.gray,
+    textAlign: "center"
+  },
+
+  detailTextStyle: {
+    fontSize: 16,
+    fontWeight: "100",
+    color: colors.gray,
+    textAlign: "center",
+    paddingVertical: 16
   },
 
   iconContainerStyle: {
@@ -61,21 +83,6 @@ export default StyleSheet.create({
     borderRadius: 50,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 40,
-  },
-
-  blankScreenHeaderTextStyle: {
-    fontSize: 17,
-    fontWeight: "600",
-    color: colors.gray,
-    textAlign: "center",
-    marginBottom: 10,
-  },
-
-  detailTextStyle: {
-    fontSize: 16,
-    color: colors.gray,
-    textAlign: "center",
-    marginVertical: 4,
-  },
+    marginBottom: 40
+  }
 });
